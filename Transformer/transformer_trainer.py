6453a5576d2bb5d363888a35c22b2502c0b72291
Ed Hirst <edward.hirst@city.ac.uk>
2025-06-12 16:15:33 +0100

    add env folder

--- Transformer/transformer_trainer.py
+++ Transformer/transformer_trainer.py
@@ -55,7 +55,7 @@ vocab = {
 vocab_size = len(vocab.keys())
 
 # Load data
-csv_file = os.path.dirname(os.getcwd())+'/Data/'+filename
+csv_file = os.getcwd()+'/Data/'+filename
 
 # Set up the dataloaders
 train_loader, val_loader = load_and_prepare_data(
@@ -76,6 +76,33 @@ optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
 print(f"Model created with {sum(p.numel() for p in model.parameters())} parameters")
 print(f"Hyperparameters: {hyperparams}")
+
+####
+
+print("Embedding layer vocab size:", model.src_embedding.num_embeddings)
+
+def get_max_token_id(loader):
+    max_token = 0
+    for batch in loader:
+        current_max = batch['input'].max().item()  # Get max token ID in batch
+        if current_max > max_token:
+            max_token = current_max
+    return max_token
+
+# Check max token IDs in both loaders
+train_max_token = get_max_token_id(train_loader)
+val_max_token = get_max_token_id(val_loader)
+
+global_max_token = max(train_max_token, val_max_token)
+vocab_size = model.src_embedding.num_embeddings
+
+print(f"Max token ID in train: {train_max_token}")
+print(f"Max token ID in val: {val_max_token}")
+print(f"Global max token ID: {global_max_token}")
+print(f"Embedding layer vocab size: {vocab_size}")
+
+#####
+
 
 # Run training
 train_losses, val_losses = train_model(
